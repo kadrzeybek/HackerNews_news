cbfabc539f235ee9e71f2a960e28f6dc67dc497e
Kadir Zeybekoğlu <119623139+kadrzeybek@users.noreply.github.com>
2023-07-27 12:09:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://pokeapi.co/"
+url = "https://news.ycombinator.com/"
 
 response = requests.get(url)
 
@@ -10,10 +10,5 @@ soup = BeautifulSoup(response.text, "html.parser")
 for link in soup.findAll('a'):
     x =  link.get('href')
     if 'http' in x:
-        try:
-            response = requests.get(x)
-            if response.status_code == 200:
-                print(response.json())
-        except:
-            print("XXXXXXXX")
+        print(x)
 
